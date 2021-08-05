64913f99dbba0fef38a5568b8963bca757af207a
Gavin Pease <gavinpease@gmail.com>
2021-08-04 21:07:57 -0600

    Add doc comments to traits

--- src/models/traits.rs
+++ src/models/traits.rs
@@ -1,15 +1,20 @@
+
+/// Gets all the traits that belong to a TV.
 pub fn tv_traits() -> Vec<&'static str > {
     vec!["action.devices.traits.OnOff", "action.devices.traits.Volume"]
 }
 
+/// Gets all the traits that belong to opening/closing doors
 pub fn open_close_traits() -> Vec<&'static str > {
     vec!["action.devices.traits.OpenClose"]
 }
 
+/// Gets all traits that belong to turning things on/off
 pub fn on_off_traits() -> Vec<&'static str > {
     vec!["action.devices.traits.OnOff"]
 }
 
+/// Gets all traits that belong to things that can be rebooted
 pub fn reboot_traits() -> Vec<&'static str > {
     vec!["action.devices.traits.Reboot"]
 }
